fe68f41a0278f8d82a5e0ceee09fde4a929860ea
Matt Dekenah <matt@freeagent.com>
2018-11-01 22:41:23 +0000

    Add palindromes app

---
+++ lib/palindromes.rb
@@ -0,0 +1,21 @@
+require 'csv'
+
+class Palindromes
+	def initialize(path_to_csv)
+		@path_to_csv = path_to_csv
+	end
+
+	def process_file
+		palindromes = []
+		CSV.foreach(@path_to_csv) do |line|
+			line.each do |word|
+				palindromes << word.strip if is_palindrome(word.strip)
+			end
+		end
+		palindromes
+	end
+
+	def is_palindrome(word)
+		word.downcase == word.downcase.reverse
+	end
+end
